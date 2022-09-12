ba9caecd90f6941e8e05b5add76263acc22e1467
ikohyppa <iko.hyppanen@gmail.com>
2022-09-12 21:29:36 +0300

    Task 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
 
 const { UserBlogs } = require('../models');
+const { tokenExtractor } = require('../util/middleware');
 
 router.post('/', async (req, res, next) => {
   const userBlogs = await UserBlogs.create(req.body);
   res.json(userBlogs);
 });
 
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  const userId = req.decodedToken.id;
+  const blogId = req.params.id;
+  const user_blogs = await UserBlogs.findOne({
+    where: { user_id: userId, blog_id: blogId },
+  });
+
+  if (user_blogs) {
+    user_blogs.read = req.body.read;
+    await user_blogs.save();
+    res.json(user_blogs);
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
